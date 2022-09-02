806c2fa8f85aaf583e8710fc21aa0e7fa00b7460
AritriBiswas <aritribiswas07@gmail.com>
2022-09-03 01:55:20 +0530

    Login page

--- schedulecreator/my-app/src/Components/Login.js
+++ schedulecreator/my-app/src/Components/Login.js
@@ -19,12 +19,12 @@ export default function Login() {
              <div className='form'>
                 <form id="email">
               
-              <input classname="email" style={{width: '270px',padding:"8px 10px",color:"black",height:"30px", borderRadius:"15px",backdropFilter:"blur(5px)", backgroundColor:"red", border:"none"}} name="email" type="email" required placeholder="Email" />
+              <input classname="box" style={{padding:"0.5em"}} name="email" type="email" required placeholder="Email" />
             </form>
 
             <form id="password">
               
-              <input classname="password" style={{width: '270px',position:"relative",padding:"8px 10px",color:"white",height:"30px", borderRadius:"15px",backdropFilter:"blur(5px)", backgroundColor:"rgb(56, 56, 56)", border:"none"}} name="password" type="password"  required placeholder="Password" />
+              <input classname="box" style={{padding:"0.5em"}} name="password" type="password"  required placeholder="Password" />
             </form>
 
             
@@ -34,7 +34,7 @@ export default function Login() {
             
             <p>Forgot password?</p>
             <p>Already using Slack?</p>
-            <a to="/Signin" style={{padding:"12px", textDecoration:"none",  margin: "12px", color:"pink"}}>Sign In to an existing workspace</a>
+            <a to="/Signin" >Sign In to an existing workspace</a>
             </div>
         </div >
         
